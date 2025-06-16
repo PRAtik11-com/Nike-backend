aa6c1f8ef2c79384cab52c075e3aa3defd2b18e0
PRAtik11-com <pratikaamrutkar9999@gmail.com>
2025-06-17 00:40:06 +0530

    vercel url added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:5174","https://nike-frontend-sooty.vercel.app"],
+  origin: ["http://localhost:5173","http://localhost:5174", "https://nike-frontend-b19bimyki-pratik-amrutkars-projects.vercel.app"],
   credentials: true
 }))
 
